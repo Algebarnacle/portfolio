a2cb1bef24cfb1edf7981c36481efa7f82573a77
Algebarnacle <gordonjosephhutchinson@gmail.com>
2024-12-12 16:34:32 -0400

    Added dropdown content to nav

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -11,7 +11,7 @@ export default function Navigation({page}:{page:string}) {
             <NavItem name="Home" selected={page == "Home"} classes="rounded-t-lg" />
             <NavDropdown name="Projects" selected={page == "Projects"} classes="" />
             <NavItem name="Background" selected={page == "Background"} classes="" />
-            <NavItem name="Resume" selected={page == "Resume"} classes="rounded-b-lg border-b-0" />
+            <NavItem name="Resume" selected={page == "Resume"} classes="rounded-b-lg border-hidden" />
         </div>
     );
 }
@@ -29,9 +29,17 @@ function NavItem({name, selected, classes}:{name:string, selected:boolean, class
 function NavDropdown({name, selected, classes}:{name:string, selected:boolean, classes:string}) {
 
     return (
-        <div className={`${selected ? "bg-primary text-secondary cursor-default" : "text-white hover:bg-primary cursor-pointer"} border-b-2 border-accent p-2 w-full text-center flex flex-row justify-between items-center ${classes}`}>
-            <div>{name}</div>
-            <FontAwesomeIcon icon={selected ? faChevronUp : faChevronDown} className="fa-fw" size="1x"/>
+        <div className="w-full">
+            <div className={`${selected ? "bg-primary text-secondary cursor-default" : "text-white hover:bg-primary cursor-pointer"} border-b-2 border-accent p-2 w-full text-center flex flex-row justify-between items-center ${classes}`}>
+                <div>{name}</div>
+                <FontAwesomeIcon icon={selected ? faChevronUp : faChevronDown} className="fa-fw" size="1x"/>
+            </div>
+            <div className="flex flex-col justify-start m-0">
+                <NavItem name="Minecraft Mod" selected={false} classes="text-xs p-1 pl-3 text-start" />
+                <NavItem name="Portfolio" selected={false} classes="text-xs p-1 pl-3 text-start" />
+                <NavItem name="Tech Roster" selected={false} classes="text-xs p-1 pl-3 text-start" />
+                <NavItem name="Link Collection" selected={false} classes="text-xs p-1 pl-3 text-start" />
+            </div>
         </div>
     );
 }
